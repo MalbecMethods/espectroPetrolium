c786dc989caea7fee101739799539b60a39fed14
solchuuxx <natalistevens1@hotmail.com>
2024-05-22 15:21:50 -0300

    Estilos para el login

--- client/src/page/Login.jsx
+++ client/src/page/Login.jsx
@@ -1,5 +1,28 @@
+import { Nav } from "../components/Nav.jsx"
+import "../../public/css/login.css"
+import UserIcon from "../../public/images/icon-user.png"
+
 export const Login = () => {
     return (
-        <div></div>
+    <>
+
+    <Nav />
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+
+            <div class="fadeIn first">
+            <img src={UserIcon} id="icon" alt="User Icon" />
+            </div>
+
+            <form>
+            <input type="text" id="login" class="fadeIn second" name="login" placeholder="User" />
+            <input type="text" id="password" class="fadeIn third" name="login" placeholder="Password" />
+            <input type="submit" class="fadeIn fourth" value="Log In" />
+            </form>
+
+        </div>
+    </div>
+    
+    </>
     )
 }
